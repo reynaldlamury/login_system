ec94db65cb10cb856cde8218c52fc407a84de1ec
reynaldlamury <51454984+reynaldlamury@users.noreply.github.com>
2023-08-15 12:15:08 +0700

    initialize sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import db from "./config/database.js";
 
 const app = express();
+
+// database connection checking ...
+try {
+  db.authenticate();
+  console.log("Database Connected ...");
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(500, () => console.log("server is running at port 5000"));
